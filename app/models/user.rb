2659885d801c05628b15a18bd617e37ad72bc55f
Louis Keller <keller01219@gmail.com>
2015-12-17 18:19:28 +0100

    user authentication

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  validates_presence_of :firstname, :lastname, :phonenumber
+  # validates_uniqueness_of :firstname, :lastname, :phonenumber
+  
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  root to: 'visitors#index'
+  devise_for :users, :controllers => {:registrations => "users/registrations"}
+  resources :users
+  # patch 'update_password', on: :collection
+  devise_scope :user do
+    get 'users/edit/password' => 'users/registrations#edit_password'
+    put 'users/edit/update_password/' => 'users/registrations#update_password'
+  end
+end
